e00879ca341ed87f002987e26dae7996ee5083ae
uuuuujin <yearn8546@gmail.com>
2022-04-26 18:04:57 +0900

    feat result modal 개발

---
+++ src/components/result-modal/result.style.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ResultModalImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 3em;
+  // background-color: pink;
+  //border-bottom: 1px solid black;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 2em 0;
+`;
