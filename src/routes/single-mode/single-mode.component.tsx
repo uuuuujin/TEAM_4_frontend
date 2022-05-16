d1e82bd46bf471920e3bb8b2989a6ac21c7df2b0
uuuuujin <yearn8546@gmail.com>
2022-05-16 23:19:35 +0900

    style 싱글모드 레이아웃 수정

--- src/routes/single-mode/single-mode.component.tsx
+++ src/routes/single-mode/single-mode.component.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import PomodoroTimer from '../../components/pomodoro-timer/pomodoro-timer.component';
 import Character from '../../components/character/character.component';
 import StateBar from '../../components/state-bar/state-bar.component';
-import CatImg from '../../assets/images/cat.png';
+import CatImg from '../../assets/images/character_cat_150px.png';
 
 import { StateBarContainer, CharacterContainer, TimerContainer } from './single-mode.style';
 
 export default function SingleMode(): JSX.Element {
   return (
     <div>
-      <TimerContainer>
-        <PomodoroTimer />
-      </TimerContainer>
-      <CharacterContainer>
-        <Character nickname="용감한푸른도마뱀!" characterImgSrc={CatImg} />
-      </CharacterContainer>
+      <div>
+        <TimerContainer>
+          <PomodoroTimer />
+        </TimerContainer>
+        <CharacterContainer>
+          <Character nickname="용감한치즈고양이" characterImgSrc={CatImg} />
+        </CharacterContainer>
+      </div>
+
       <StateBarContainer>
         <StateBar>내 새끼 밥주는 중...</StateBar>
       </StateBarContainer>
