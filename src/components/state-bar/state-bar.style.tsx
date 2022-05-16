3a460cbd74c4703abd21f92308dcc2bc9efa1a42
uuuuujin <yearn8546@gmail.com>
2022-05-16 18:41:45 +0900

    style 상태바 스타일 변경

--- src/components/state-bar/state-bar.style.tsx
+++ src/components/state-bar/state-bar.style.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export const StateBarContainer = styled.span`
-  padding: 1em;
-  border: 2px solid black;
+  padding: 1em 2em;
+  border: 2px solid #f43f59;
   border-radius: 5px;
   display: inline-block;
-  background-color: #c4c4c4;
+  background-color: #ffcad5;
+  letter-spacing: 0.1rem;
 `;
