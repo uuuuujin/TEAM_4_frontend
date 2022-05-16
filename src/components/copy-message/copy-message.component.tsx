e2698bf09c53042b5f26c5a00d4612faf5027989
uuuuujin <yearn8546@gmail.com>
2022-05-16 21:25:54 +0900

    style 링크 복사완료 메세지 스타일 변경

--- src/components/copy-message/copy-message.component.tsx
+++ src/components/copy-message/copy-message.component.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { StateBarContainer } from './copy-message.style';
 
 export default function CopyMsg(): JSX.Element {
-  return <StateBarContainer>링크를 클립보드에 복사했습니다.</StateBarContainer>;
+  return <StateBarContainer>클립보드에 복사되었습니다.</StateBarContainer>;
 }
--- src/components/copy-message/copy-message.style.tsx
+++ src/components/copy-message/copy-message.style.tsx
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
-export const StateBarContainer = styled.div`
-  width: 250px;
-  padding: 1em;
+export const StateBarContainer = styled.span`
+  display: inline-block;
+  padding: 0.5em 1em;
+  border: 1px solid #f43f59;
   border-radius: 5px;
-  background-color: #4d4d4d;
-  color: #fff;
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
+  background-color: #ffcad5;
+  letter-spacing: 0.08rem;
 
   animation: fadein 1s;
 
