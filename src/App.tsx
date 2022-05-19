707328e27ec1ea4e693807ae844cb8bf28654b21
uuuuujin <yearn8546@gmail.com>
2022-05-19 22:59:17 +0900

    style 배경 이미지 삽입 & 타이머 크기 조절

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Main from './routes/main/main.component';
 import SingleMode from './routes/single-mode/single-mode.component';
 import MultiMode from './routes/multi-mode/multi-mode.component';
 import Navigation from './components/navigation/navigation.component';
+import './App.css';
 
 export default function App(): JSX.Element {
   return (
