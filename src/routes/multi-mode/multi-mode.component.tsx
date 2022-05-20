44cd7961aff202f64cb98ab6cfbaf8034b426f2b
uuuuujin <yearn8546@gmail.com>
2022-05-20 15:56:54 +0900

    style 멀티모드 레이아웃

--- src/routes/multi-mode/multi-mode.component.tsx
+++ src/routes/multi-mode/multi-mode.component.tsx
@@ -1,8 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/index.hook';
 import { createRoomAsync, enterRoomAsync, multiAction } from '../../store/modules/multi/multi.slice';
 import { selectIsEntered, selectRoomId, selectIsConnected } from '../../store/modules/multi/multi.select';
+import {
+  TimerContainer,
+  ChracterPosition,
+  ButtonContainer,
+  LinkContainer,
+  GuidanceText,
+  CopyMsgContainer,
+  StateBarContainer,
+} from './multi-mode.style';
+import PomodoroTimer from '../../components/pomodoro-timer/pomodoro-timer.component';
+import catImg from '../../assets/images/character_cat_150px.png';
+import Character from '../../components/character/character.component';
+import Button from '../../components/button/button.component';
+import MultiLink from '../../components/multi-link/multi-link.component';
+import ToastHook from '../../hooks/toast.hook';
+import CopyMsg from '../../components/copy-message/copy-message.component';
+import StateBar from '../../components/state-bar/state-bar.component';
 
 export default function MultiMode(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -41,5 +58,50 @@ export default function MultiMode(): JSX.Element {
       dispatch(multiAction.startConnectSocket());
     }
   }, [dispatch, isEntered]);
-  return <div>{!isConnected && <Outlet />}</div>;
+
+  const users = [
+    { position: 1, nickname: '유진', characterImg: catImg },
+    { position: 2, nickname: '성훈', characterImg: catImg },
+    { position: 3, nickname: '은우', characterImg: catImg },
+    { position: 4, nickname: '현석', characterImg: catImg },
+    { position: 5, nickname: '보민', characterImg: catImg },
+    { position: 6, nickname: '유천', characterImg: catImg },
+  ];
+
+  const [toastState, setToastState] = useState<boolean>(false);
+  ToastHook(toastState, setToastState);
+
+  return (
+    <div>
+      {!isConnected && <Outlet />}
+      <TimerContainer>
+        <PomodoroTimer />
+      </TimerContainer>
+      {users.map((item) => {
+        return (
+          <ChracterPosition positionNum={item.position}>
+            <Character nickname={item.nickname} characterImgSrc={item.characterImg} />
+          </ChracterPosition>
+        );
+      })}
+      <ButtonContainer>
+        <Button>시작하기</Button>
+      </ButtonContainer>
+
+      <GuidanceText>링크를 보내 친구들과 함꼐하자!</GuidanceText>
+      <LinkContainer>
+        <MultiLink setToastState={setToastState}>http://podongpodong.com/mode/friends/psa-hjjr-mwk</MultiLink>
+      </LinkContainer>
+      {toastState && (
+        <CopyMsgContainer>
+          <CopyMsg />
+        </CopyMsgContainer>
+      )}
+
+      {/* 이건 상태바 */}
+      {/* <StateBarContainer>
+        <StateBar>집중하는 중...</StateBar>
+      </StateBarContainer> */}
+    </div>
+  );
 }
