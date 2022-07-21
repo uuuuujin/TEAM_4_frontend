bfe38ea446dc749ce50b76ed4e094ecb4af974e3
Be-student <presentsong@naver.com>
2022-07-21 22:05:39 +0900

    링크 복사 안되던 상황 수정

--- src/components/multi-link/multi-link.component.tsx
+++ src/components/multi-link/multi-link.component.tsx
@@ -4,34 +4,19 @@ interface LinkType {
   children: string;
   setToastState: (value: boolean) => void;
 }
-
 export default function MultiLink({ children, setToastState }: LinkType): JSX.Element {
-  const doCopy = (url: string) => {
-    const textarea = document.createElement('textarea');
-    textarea.value = url;
-    textarea.style.top = '0';
-    textarea.style.left = '0';
-    textarea.style.position = 'fixed';
-
-    document.body.appendChild(textarea);
-    textarea.focus();
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-  };
-
-  const handlerCopy = (url: string) => {
+  const handlerCopy = () => {
     setToastState(true);
-    doCopy(url);
+    navigator.clipboard.writeText(window.location.href);
   };
 
   return (
     <LinkContainer>
-      <CopyBtn onClick={() => handlerCopy(children)}>복사</CopyBtn>
-
       <LinkAddressContainer>
         <LinkAddress>{children.slice(0, 40)}...</LinkAddress>
       </LinkAddressContainer>
+
+      <CopyBtn onClick={handlerCopy}>복사</CopyBtn>
     </LinkContainer>
   );
 }
